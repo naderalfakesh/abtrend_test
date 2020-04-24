da2cbf5df2f5b364ae05d52bc34ba57cc95220cc
naderalfakesh <naderalfakesh@gmail.com>
2020-04-25 01:10:40 +0300

    main section responsive and js small fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", slider);
 
-function slider(){
-    // attach a click event listner on slides list
-    const slides = document.querySelector(".productInfo__list");
-    slides.addEventListener('click',replaceTargetSlide);
-
+function slider() {
+  // attach a click event listner on slides list
+  const slides = document.querySelector(".productInfo__list");
+  slides.addEventListener("click", replaceTargetSlide);
 }
 
-function replaceTargetSlide(event){
-    const targetSlide =  document.querySelector("img.targetSlide");
-    event.preventDefault();
+function replaceTargetSlide(event) {
+  const targetSlide = document.querySelector("img.targetSlide");
+  event.preventDefault();
+  // check if the target is image or list
+  if (event.target.tagName === "IMG") {
     const imgsrc = event.target.src;
     targetSlide.src = imgsrc;
-
+  }
 }
